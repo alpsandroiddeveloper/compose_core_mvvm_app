591fe97110b469cf66cc7996b10f528f3413b601
Alps <alpeshk1998@gmail.com>
2025-08-19 21:07:15 +0530

    This is first commit.

---
+++ app/src/main/java/com/alps/compose_mvvm_strcucture/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.alps.compose_mvvm_strcucture
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.alps.compose_mvvm_strcucture.ui.theme.Compose_MVVM_StrcuctureTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Compose_MVVM_StrcuctureTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Compose_MVVM_StrcuctureTheme {
+        Greeting("Android")
+    }
+}
